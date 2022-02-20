f22cfb2fd620386f92d7225b14c6c14d30eb3997
TacoSauceDev <86977366+TacoSauceDev@users.noreply.github.com>
2022-02-19 18:34:03 -0700

    Update initial_test.py

--- tests/initial_test.py
+++ tests/initial_test.py
@@ -11,12 +11,3 @@ def test_extractSecret():
 
 def test_buildResponse():
   assert (buildResponse("123","321","200")) == ({"statusCode": "200","headers": "321","body": "123"})
-  
-  
-  
-  def buildResponse(body_payload, headers, response_code):
-    return {
-        "statusCode": response_code,
-        "headers": headers,
-        "body": body_payload
-    }
