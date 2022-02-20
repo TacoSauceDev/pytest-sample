ec05c8b26d5626039f2f21efc31412795af45a72
TacoSauceDev <86977366+TacoSauceDev@users.noreply.github.com>
2022-02-19 17:51:30 -0700

    Testing running function from tests

--- tests/initial_test.py
+++ tests/initial_test.py
@@ -1,3 +1,12 @@
-
 def test_passing():
+  assert (1,2,3) == (1,2,3)
+
+def test_retrieveSecret():
+  assert (1,2,3) == (1,2,3)
+
+def test_extractSecret():
+  assert (1,2,3) == (1,2,3)
+
+def test_buildResponse():
+  print(buildResponse("123","123","123"))
   assert (1,2,3) == (1,2,3)
