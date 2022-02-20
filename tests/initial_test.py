ac3a57762ace00bebbecedf98e8457e0f791a3f0
TacoSauceDev <86977366+TacoSauceDev@users.noreply.github.com>
2022-02-19 18:02:20 -0700

    Testing2

--- tests/initial_test.py
+++ tests/initial_test.py
@@ -1,5 +1,3 @@
-from index import buildResponse
-
 def test_passing():
   assert (1,2,3) == (1,2,3)
 
@@ -10,5 +8,6 @@ def test_extractSecret():
   assert (1,2,3) == (1,2,3)
 
 def test_buildResponse():
+  import index
   print(buildResponse("123","123","123"))
   assert (1,2,3) == (1,2,3)
