9f440c7f6ca98cbbe3cf28481ac2df626faf862b
TacoSauceDev <86977366+TacoSauceDev@users.noreply.github.com>
2022-02-19 17:33:05 -0700

    Basic application code.

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # pytest-sample
+
+Simple Python Lambda Function that gets a secret value from Secrets manager based on environment variables set on the runner. 
+
+This is for an example of how to do python testing with a mocking secrets manager and pytest.
---
+++ index.py
@@ -0,0 +1,33 @@
+# Purpose: To retrieve a secret from AWS Secrets Manager assuming you are running it from inside a lambda function
+# This function is exceptionally verbose to give multiple points to unit test for the example.
+
+import boto3 
+import json 
+import os
+
+# Inputs -- you should have three variables come in from os.environ variables --
+# os.environ('secretId')
+# os.environ('secretValueName')
+# os.environ('headerValues')
+
+client = boto3.client('secretsmanager')
+
+def handler(event, context):
+    secret = retrieveSecret(os.environ['secretId'])
+    secretExtracted = extractSecret(secret, os.environ['secretValueName'])
+    return buildResponse(secretExtracted, os.environ['headerValues'], '200')
+
+def retrieveSecret(secretId):
+    return this.client.get_secret_value(SecretId=secretId)
+
+def extractSecret(secret, secretValueName):
+    secrets = json.loads(secret['SecretString'])
+    return secrets[secretValueName]
+
+
+def buildResponse(body_payload, headers, response_code):
+        return {
+        "statusCode": response_code,
+        "headers": headers,
+        "body": body_payload
+        }
